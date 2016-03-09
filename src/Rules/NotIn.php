4acf7852e4683d5033a48b6215cc5eb9777266b4
Daison Carino <daison12006013@gmail.com>
2016-03-09 15:20:54 +0000

    negating rule In using respect validation 'Not' and 'In'

--- src/Rules/NotIn.php
+++ src/Rules/NotIn.php
@@ -10,15 +10,15 @@ class NotIn extends BaseRule
             return true;
         }
 
-        $value = $this->getParams()[1];
+        $input = trim($this->getParams()[1]);
 
-        foreach (explode(',', $this->getParams()[2]) as $elem) {
-            if ($value == trim($elem)) {
-                return false;
-            }
-        }
+        $values = array_map(function($elem) {
+            return trim($elem);
+        }, explode(',', $this->getParams()[2]));
 
-        return true;
+        $in = $this->respect('In', [$values]);
+
+        return $this->respect('Not', [$in])->validate($input);
     }
 
     public function getMessage()
